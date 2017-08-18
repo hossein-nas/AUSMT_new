621ecd325b172ba5492f6f3cb8d00caac56e3d85
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2017-08-18 17:31:49 +0430

    Yek meta tag be header master page ezafe shode baraie taghir rang dar chrome

--- resources/views/layouts/site_master.blade.php
+++ resources/views/layouts/site_master.blade.php
@@ -10,7 +10,8 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
 	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-
+	<meta name="theme-color" content="#C91021">
+	
 </head>
 <body>
 
