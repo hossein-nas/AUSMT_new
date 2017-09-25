e1a98c2a687733cebf2beed32961ecc210fe2323
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2017-09-25 15:37:20 +0330

    Stop Traching vendor directory 	- code reformating add gulffile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,61 @@
-var gulp = require ('gulp');
-var sass = require ('gulp-sass');
-var autoprefixer = require ('gulp-autoprefixer');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
 
 var paths = {
-    site : {
+    site: {
         dest: "public/assets/css",
         src: "resources/assets/sass/main.scss",
-        watch_list: ['resources/assets/sass/partials/site/**/*.scss','resources/assets/sass/main.scss']
+        watch_list: ['resources/assets/sass/partials/site/**/*.scss', 'resources/assets/sass/main.scss']
     },
-    cpanel : {
+    cpanel: {
         dest: "public/assets/css",
         src: "resources/assets/sass/cpanel.scss",
-        watch_list: ['resources/assets/sass/partials/cpanel/**/*.scss','resources/assets/sass/cpanel.scss']
+        watch_list: ['resources/assets/sass/partials/cpanel/**/*.scss', 'resources/assets/sass/cpanel.scss']
     },
     jsFiles: {
-        dest : "public/assets/js",
-        jquery : "resources/assets/jsFiles/jq.js",
-        frameworks : "resources/assets/jsFiles/frameworks/*.js",
-        custom : "resources/assets/jsFiles/custom/*.js",
+        dest: "public/assets/js",
+        jquery: "resources/assets/jsFiles/jq.js",
+        frameworks: "resources/assets/jsFiles/frameworks/*.js",
+        custom: "resources/assets/jsFiles/custom/*.js"
     }
-}
+};
 
-gulp.task('sass',function(){
+gulp.task('sass', function () {
     gulp.src(paths.site.src)
         .pipe(
-            sass().on("error",gutil.log)
+            sass().on("error", gutil.log)
         )
         .pipe(autoprefixer('last 100 version'))
         .pipe(gulp.dest(paths.site.dest));
     gulp.src(paths.site.src)
         .pipe(
-            sass({outputStyle: 'compressed'}).on("error",gutil.log)
+            sass({outputStyle: 'compressed'}).on("error", gutil.log)
         )
         .pipe(autoprefixer('last 100 version'))
         .pipe(rename('main.min.css'))
         .pipe(gulp.dest(paths.site.dest));
     gulp.src(paths.cpanel.src)
         .pipe(
-            sass().on("error",gutil.log)
+            sass().on("error", gutil.log)
         )
         .pipe(autoprefixer('last 100 version'))
         .pipe(gulp.dest(paths.cpanel.dest));
     gulp.src(paths.cpanel.src)
         .pipe(
-            sass({outputStyle: 'compressed'}).on("error",gutil.log)
+            sass({outputStyle: 'compressed'}).on("error", gutil.log)
         )
         .pipe(autoprefixer('last 100 version'))
         .pipe(rename('cpanel.min.css'))
         .pipe(gulp.dest(paths.cpanel.dest));
 });
 
- 
-gulp.task('minify',function(){
+
+gulp.task('minify', function () {
     gulp.src(paths.jsFiles.jquery)
         .pipe(uglify())
         .pipe(gulp.dest(paths.jsFiles.dest));
@@ -70,14 +70,14 @@ gulp.task('minify',function(){
         .pipe(gulp.dest(paths.jsFiles.dest))
         .pipe(rename('scripts.min.js'))
         .pipe(
-            uglify().on('error',gutil.log)
+            uglify().on('error', gutil.log)
         )
         .pipe(gulp.dest(paths.jsFiles.dest));
 });
 
-gulp.task('watch',function(){
-    gulp.watch([ paths.cpanel.watch_list, paths.site.watch_list ],['sass']);
+gulp.task('watch', function () {
+    gulp.watch([paths.cpanel.watch_list, paths.site.watch_list], ['sass']);
     gulp.watch('resources/assets/jsFiles/**/*.js', ['minify']);
 });
 
-gulp.task('default', ['sass','minify','watch']);
+gulp.task('default', ['sass', 'minify', 'watch']);
