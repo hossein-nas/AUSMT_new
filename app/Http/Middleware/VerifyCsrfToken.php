7ca15dc1df5f0830570dee42313d10fa763c039b
hossein-nas <hossein.nasiri.sovari@gmail.com>
2017-12-02 02:12:26 +0330

    Working on post new page

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+        'upload/images/',
+        'upload/record/thumbnail/'
     ];
 }
