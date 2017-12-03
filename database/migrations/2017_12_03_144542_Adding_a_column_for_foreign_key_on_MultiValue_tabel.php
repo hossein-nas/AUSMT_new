b01a51aa94af246d80d8bce0054e3af24aa6b232
hossein-nas <hossein.nasiri.sovari@gmail.com>
2017-12-03 14:43:08 +0330

    Fixing some issue on File_MultiValue table

---
+++ database/migrations/2017_12_03_144542_Adding_a_column_for_foreign_key_on_MultiValue_tabel.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddingAColumnForForeignKeyOnMultiValueTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('au_file_multivalue', function (Blueprint $table) {
+            $table->integer('related_file_id')->unsigned()->after('id');
+            $table->dropForeign('id');
+            $table->foreign('related_file_id')
+                ->references('id')
+                ->on('au_file');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('au_file_multivalue', function (Blueprint $table) {
+            $table->dropForeign('related_file_id');
+            $table->dropColumn('related_file_id');
+        });
+    }
+}
